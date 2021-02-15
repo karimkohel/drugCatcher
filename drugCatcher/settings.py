0c2ed9fa3bb2d4ab52707d19d47041e5bc7867d2
karim kohel <kareemkohel@gmail.com>
2021-02-15 19:05:43 +0200

    static files management (dev)

--- drugCatcher/settings.py
+++ drugCatcher/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 
 
 # STATIC
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
