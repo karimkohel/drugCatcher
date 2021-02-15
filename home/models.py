5b677052366aa96bd35c44f4a56fe587ddbb969d
karim kohel <kareemkohel@gmail.com>
2021-02-15 22:54:24 +0200

    added comany and country models

--- home/models.py
+++ home/models.py
@@ -1,8 +1,33 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+# Django's Moto is : Keep your Models fat and your Views small
+# and i did just that
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Company(models.Model):
+    name = models.CharField(max_length=150)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 class Appointment(models.Model):
+
     firstName = models.CharField(max_length=30, help_text="First Name")
     lastName = models.CharField(max_length=30, help_text="Last Name")
     email = models.EmailField(help_text="Work Email Address")
+    phoneNumber = PhoneNumberField()
+    country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
 
+
+    
+    def __str__(self):
+        return self.firstName + " " + self.lastName
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from .models import Appointment
 
 # Create your views here.
 
@@ -7,3 +9,10 @@ def index(request):
 
 def appointmentReq(request):
     return render(request, 'home/appReq.html')
+
+# or better to do 
+
+# class CreateAppointment(CreateView):
+#     model = Appointment
+
+# and create an ajax view to change company 
