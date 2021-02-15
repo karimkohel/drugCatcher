167f6937494acc02b1ed63bcbe61dc469b1cf50a
karim kohel <kareemkohel@gmail.com>
2021-02-15 16:33:22 +0200

    basic templates setup

--- drugCatcher/urls.py
+++ drugCatcher/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from home.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', )
+    path('/', include('home.urls'))
 ]
